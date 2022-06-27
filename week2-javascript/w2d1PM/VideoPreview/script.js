da9c0165a95b41b58d4b9d2639dfee6d88d23206
bman9814 <bman9814@gmail.com>
2022-06-27 17:44:33 -0400

    video-preview version 1

---
+++ week2-javascript/w2d1PM/VideoPreview/script.js
@@ -0,0 +1,9 @@
+console.log("page loaded...");
+function playVideo(element){
+    console.log("working");
+    element.play();
+}
+function pauseVideo(element){
+    console.log("working");
+    element.pause();
+}
