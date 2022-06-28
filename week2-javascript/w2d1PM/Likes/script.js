a090b436534ea5a58dda152e45276638cd34113e
bman9814 <bman9814@gmail.com>
2022-06-27 20:12:17 -0400

    Likes Version 1

---
+++ week2-javascript/w2d1PM/Likes/script.js
@@ -0,0 +1,16 @@
+// var likes1 = document.querySelector("#span1");
+function increaselikes1(){
+    console.log("working");
+    likes1 = document.querySelector("#span1");
+    likes1.innerText++;
+}
+function increaselikes2(){
+    console.log("working");
+    likes2 = document.querySelector("#span2");
+    likes2.innerText++; 
+}
+function increaselikes3(){
+    console.log("working");
+    likes3 = document.querySelector("#span3");
+    likes3.innerText++;
+}
