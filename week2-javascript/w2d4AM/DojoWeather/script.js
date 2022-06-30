dfcb23180cffc1a4b7571a4a061d93016896b61a
bman9814 <bman9814@gmail.com>
2022-06-30 17:59:03 -0400

    Dojo Weather version 1

--- week2-javascript/w2d4AM/DojoWeather/script.js
+++ week2-javascript/w2d4AM/DojoWeather/script.js
@@ -5,20 +5,22 @@ function cookiemonster(){
 }
 function TempChange(){
     temp = document.querySelector("#tempmenu");
-    arr = [document.querySelector(".num1").innerText, document.querySelector(".num2").innerText, document.querySelector(".num3").innerText, document.querySelector(".num4").innerText, document.querySelector(".num5").innerText, document.querySelector(".num6").innerText, document.querySelector(".num7").innerText, document.querySelector(".num8").innerText];
+    arr = [document.querySelector(".num0").innerText, document.querySelector(".num1").innerText, document.querySelector(".num2").innerText, document.querySelector(".num3").innerText, document.querySelector(".num4").innerText, document.querySelector(".num5").innerText, document.querySelector(".num6").innerText, document.querySelector(".num7").innerText];
     arr = arr.map(Number);
     console.log(arr);
     if(temp.value == "celcius"){
         console.log("working");
         for(i=0;i<arr.length;i++){
-            arr[i] = arr[i] * 9 / 5 + 32;
+            arr[i] = (arr[i] - 32) / 1.8;
+            arr[i] = Math.round(arr[i]);
             document.querySelector(".num" + i).innerHTML = arr[i];
         }
     }
     if(temp.value == "Farenheight"){
         console.log("working2");
         for(i=0;i<arr.length;i++){
-            arr[i] = (arr[i] - 32) * 5 / 9;
+            arr[i] = arr[i] * 1.8 + 32;
+            arr[i] = Math.round(arr[i]);
             document.querySelector(".num" + i).innerHTML = arr[i];
         }
     }
